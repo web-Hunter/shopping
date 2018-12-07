16738a51be575d8068a6b6eca98eadfd9c3b8310
蒋岩 <279646563@qq.com>
2018-12-07 13:26:45 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,12 @@
             
 console.log("122333")
 //var oTable = '<table class="table tables"><tr><th>拜托项目</th><th>2018目标</th><th>累计实现</th><th>达成率</th></tr><tr><td><span>合同收款</span></td><td class="zt_dq"><span id="sk_mubiao">-</span></td><td class="zt_dq"><span id="sk_leiji">-</span></td><td class="zt_dq"><span id="sk_dacheng">-</span></td></tr><tr><td><span>产品结算</span></td><td class="zt_dq"><span id="js_mubiao">-</span></td><td class="zt_dq"><span id="js_leiji">-</span></td><td class="zt_dq"><span id="js_dacheng">-</span></td></tr></table>'
+
+
+
+
+window.cp = function () {
+mbos.eas.invokeScript("getDatas",['u1IAAAAKl/L5CwEz','201812'],function(data){
+  console.log(data)
+})
+}
