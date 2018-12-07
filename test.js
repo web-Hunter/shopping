edcec321cdc6a9d2629e58f5f0480493f5fdacfd
蒋岩 <279646563@qq.com>
2018-12-07 11:14:05 +0800

    45
    4

---
+++ test.js
@@ -0,0 +1,5 @@
+mbos.eas.invokeScript("getDatas",[adminnumber,today],function(data){
+  console.log(data)
+})
+            
+//var oTable = '<table class="table tables"><tr><th>拜托项目</th><th>2018目标</th><th>累计实现</th><th>达成率</th></tr><tr><td><span>合同收款</span></td><td class="zt_dq"><span id="sk_mubiao">-</span></td><td class="zt_dq"><span id="sk_leiji">-</span></td><td class="zt_dq"><span id="sk_dacheng">-</span></td></tr><tr><td><span>产品结算</span></td><td class="zt_dq"><span id="js_mubiao">-</span></td><td class="zt_dq"><span id="js_leiji">-</span></td><td class="zt_dq"><span id="js_dacheng">-</span></td></tr></table>'
